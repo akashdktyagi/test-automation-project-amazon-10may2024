b3acc4cf67124d4e9bbf49c9334a5325ba60e564
akashdktyagi <akashdktyagi@gmail.com>
2024-06-16 10:58:49 +0800

    CI file

--- reusable-component-demo/src/test/java/com/club/Runner.java
+++ reusable-component-demo/src/test/java/com/club/Runner.java
@@ -1,26 +1,26 @@
-package com.club;
+//package com.club;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
-
-@CucumberOptions(
-        features = {"classpath:features"},
-        glue = {"org.tdoc.stepdefs"},
-        plugin = {"pretty",
-                "html:target/cucumber-reports/cucumber-pretty.html",
-                "json:target/cucumber-reports/CucumberTestReport.json",
-                "rerun:target/cucumber-reports/rerun.txt",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-        },
-        monochrome = true,
-        tags = ""
-)
-public class Runner extends AbstractTestNGCucumberTests{
-
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-        return super.scenarios();
-    }
-}
+////import io.cucumber.testng.AbstractTestNGCucumberTests;
+////import io.cucumber.testng.CucumberOptions;
+////import org.testng.annotations.DataProvider;
+//
+////@CucumberOptions(
+////        features = {"classpath:features"},
+////        glue = {"org.tdoc.stepdefs"},
+////        plugin = {"pretty",
+////                "html:target/cucumber-reports/cucumber-pretty.html",
+////                "json:target/cucumber-reports/CucumberTestReport.json",
+////                "rerun:target/cucumber-reports/rerun.txt",
+////                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+////        },
+////        monochrome = true,
+////        tags = ""
+////)
+//public class Runner {\\extends AbstractTestNGCucumberTests{
+//
+//    @Override
+//    @DataProvider(parallel = true)
+//    public Object[][] scenarios() {
+//        return super.scenarios();
+//    }
+//}
